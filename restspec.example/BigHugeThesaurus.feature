87ec494c8580f5eea655c9f8de341e719ba21e1c
Matt Hammond <matt@softwired.co.uk>
2015-07-15 23:19:49 +0100

    Add specflow feature and initial steps

---
+++ restspec.example/BigHugeThesaurus.feature
@@ -0,0 +1,10 @@
+﻿Feature: BigHugeThesaurus
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: JSON response from Big Huge Labs Thesaurus API
+	When I issue a 'GET' request to 'http://words.bighugelabs.com/apisample.php?v=2&format=json'
+	Then the result should be '200'
+	And the response should be valid JSON
---
+++ restspec/StepDefinitions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace restspec
+{
+    public class StepDefinitions
+    {
+        [When(@"I issue a '(.*)' request to '(.*)'")]
+        public void WhenIIssueARequestTo(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result should be '(.*)'")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the response should be valid JSON")]
+        public void ThenTheResponseShouldBeValidJSON()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
