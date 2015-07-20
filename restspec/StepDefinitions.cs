8243260d5eda7af99cd1b0753cbad0028f44f98f
Matt Hammond <matt@softwired.co.uk>
2015-07-20 22:22:55 +0100

    Add basic working API support

--- restspec/StepDefinitions.cs
+++ restspec/StepDefinitions.cs
@@ -1,30 +1,69 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Should;
+using Should.Core.Assertions;
 using TechTalk.SpecFlow;
 
 namespace restspec
 {
-    public class StepDefinitions
+
+    public class TestContext
     {
-        [When(@"I issue a '(.*)' request to '(.*)'")]
-        public void WhenIIssueARequestTo(string p0, string p1)
+        private static readonly WebRequestHandler WebRequestHandler = new WebRequestHandler
         {
-            ScenarioContext.Current.Pending();
+            CachePolicy =
+                            new HttpRequestCachePolicy(
+                            HttpCacheAgeControl.MaxAgeAndMinFresh,
+                            TimeSpan.FromMilliseconds(0),
+                            TimeSpan.FromMilliseconds(0))
+        };
+
+        public HttpClient GetHttpClient()
+        {
+
+            return new HttpClient(WebRequestHandler)
+            {
+            };
         }
 
+        public HttpRequestMessage RequestMessage { get; } 
+        public HttpResponseMessage ResponseMessage { get; set; } 
+    }
+
+    [Binding]
+    public class StepDefinitions
+    {
+        private readonly TestContext _testContext;
+        public StepDefinitions(TestContext testContext)
+        {
+            _testContext = testContext;
+        }
+
+        [When(@"I issue a '(.*)' request to '(.*)'")]
+        public void WhenIIssueARequestTo(string httpMethod, string url)
+        {
+            var client = _testContext.GetHttpClient();
+            var task = client.SendAsync(new HttpRequestMessage(new HttpMethod(httpMethod), url));
+            Task.WaitAll(task);
+            _testContext.ResponseMessage = task.Result;
+        }
+         
         [Then(@"the result should be '(.*)'")]
         public void ThenTheResultShouldBe(int p0)
         {
-            ScenarioContext.Current.Pending();
+            
         }
 
         [Then(@"the response should be valid JSON")]
-        public void ThenTheResponseShouldBeValidJSON()
+        public void ThenTheResponseShouldBeValidJson()
         {
-            ScenarioContext.Current.Pending();
+           
         }
 
     }
